99f75ad5e6502e770827061322568d4aff1f7c0a
Levi Njoroge <Levynjoroge1@gmail.com>
2025-04-02 14:45:45 +0300

    learning node.js from w3schools

--- nodejs_fr_w3schools/3 demo_module.js
+++ nodejs_fr_w3schools/3 demo_module.js
@@ -1,2 +1,16 @@
 var http = require('http');
 var dt = require('./myfirstmodule');
+
+http.createServer(function (req, res) {
+    //this is the head of the response
+    res.writeHead(
+        200, 
+        {
+        'Content-Type': 'text/html'
+        });
+        
+    //this is the body of the response
+    res.write()    
+}
+
+)
