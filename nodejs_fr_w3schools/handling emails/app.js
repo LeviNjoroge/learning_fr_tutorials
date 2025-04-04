ec60222652e0efb5a10ff918e6e8cd879d89fcce
Levi Njoroge <Levynjoroge1@gmail.com>
2025-04-04 14:18:38 +0300

    working on emails with nodejs

--- nodejs_fr_w3schools/handling emails/app.js
+++ nodejs_fr_w3schools/handling emails/app.js
@@ -1,4 +1,30 @@
 require('dotenv').config();
+const nodemailer = require('nodemailer');
 
 const email_address = process.env.EMAIL_ADDRESS;
 const password = process.env.PASSWORD;
+
+//receivers email
+var mail = "levynjoroge1@gmail.com";
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: email_address,
+        pass: password
+    }
+})
+
+var mailOptions = {
+    from: email_address,
+    to: mail,
+    subject: "Sending an Email via Node.js",
+    text:"This should be easy"
+}
+
+transporter.sendEmail(mailOptions, (err, info)=>{
+    if (err) throw err;
+    else{
+        console.log("Email sent "+info.response);
+    }
+})
