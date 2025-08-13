b6d87673528f625aa65499a5469550565d4c9a0d
Levi Njoroge <Levynjoroge1@gmail.com>
2025-08-13 14:24:02 +0300

    creating fun react projects while learning

--- react_fr_brocode/card_components_app/src/Card.jsx
+++ react_fr_brocode/card_components_app/src/Card.jsx
@@ -1,9 +1,11 @@
+import profile_pic from "./assets/render.png"
+
 function Card() {
     return(
         <div className="card">
-            <img src="https://via.placeholder.com/150" alt="profile picture" />
-            <h2>Jun Lee</h2>
-            <p>I'm a Software Engineering student and love playing PUBG and Chess</p>
+            <img src={profile_pic} alt="profile picture"/>
+            <h2 className="card_heading">Jun Lee</h2>
+            <p className="card_text">I'm a Software Engineering student and love playing PUBG and Chess</p>
         </div>
     );
 }
