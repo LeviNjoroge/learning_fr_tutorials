069664096c6706625ad742d8c7ae37ef3def4ba9
Levi Njoroge <Levynjoroge1@gmail.com>
2025-08-13 13:57:03 +0300

    creating fun react projects while learning

---
+++ react_fr_brocode/card_components_app/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+    return(
+        <div className="card">
+            <img src="https://via.placeholder.com/150" alt="profile picture" />
+            <h2>Jun Lee</h2>
+            <p>I'm a Software Engineering student and love playing PUBG and Chess</p>
+        </div>
+    );
+}
+
+export default Card
