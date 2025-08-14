5a8bf43f352b1862c3af4e289457e0185305f7df
Levi Njoroge <Levynjoroge1@gmail.com>
2025-08-14 22:20:43 +0300

    working on react

---
+++ react_fr_brocode/learning_props/src/Student.jsx
@@ -0,0 +1,10 @@
+// for props I should use "props" object as a parameter and access the data stored using key:value pairs
+
+function Student(props) {
+    
+    return(
+
+    );
+}
+
+export dedfaku
