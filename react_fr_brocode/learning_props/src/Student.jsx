3336637219aa4fef24cf9c8a345155c45fe40588
Levi Njoroge <Levynjoroge1@gmail.com>
2025-08-16 13:13:26 +0300

    learning react

--- react_fr_brocode/learning_props/src/Student.jsx
+++ react_fr_brocode/learning_props/src/Student.jsx
@@ -6,7 +6,7 @@ function Student(props) {
         <div className="student">
             <p>Name: {props.name} <br />
                 Age: {props.age} <br />
-                <span title={props.title}>Course: {props.course}</span> <br />
+                <span title={props.title}>Course: {props.course}</span> <br/>
             </p>
         </div>
     );
