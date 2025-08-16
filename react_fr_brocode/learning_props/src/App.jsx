321c76fe46c0ce3b6af107d72cf680b0557fa14b
Levi Njoroge <Levynjoroge1@gmail.com>
2025-08-16 14:22:35 +0300

    learning react

--- react_fr_brocode/learning_props/src/App.jsx
+++ react_fr_brocode/learning_props/src/App.jsx
@@ -1,14 +1,13 @@
 import Student from "./Student";
-
 function App(){
-
   return(
     <>
       <Student name="Spongebob" age={30} course="Food Processing"/>
       <Student name="Patrick" age={36} course="Fishology" title="if there exists LOL"/>
       <Student name="Sandy" age={20} course="BSc. Physics"/>
+      <Student/>
+      <Student name="Squidward" age="90" course="Music"/>
     </>
   );
 }
-
 export default App
--- react_fr_brocode/learning_props/src/Student.jsx
+++ react_fr_brocode/learning_props/src/Student.jsx
@@ -1,4 +1,5 @@
 // for props I should use "props" object as a parameter and access the data stored using key:value pairs
+import Proptypes from 'prop-types'
 
 function Student(props) {
     
@@ -10,6 +11,18 @@ function Student(props) {
             </p>
         </div>
     );
+}
+
+// Student.propTypes = {
+//     name: propTypes.string,
+//     age: Proptypes.number,
+//     course: propTypes.string
+// }
+
+Student.defaultProps = {
+    name: "Guest",
+    age: 0,
+    Course: "None Selected"
 }
 
 export default Student
