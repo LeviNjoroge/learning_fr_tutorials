52081e985eaa7d3a8922d92b49522246832691c6
Levi Njoroge <Levynjoroge1@gmail.com>
2025-08-25 23:01:19 +0300

    working on react

--- react_fr_w3/my-react-app/src/main.jsx
+++ react_fr_w3/my-react-app/src/main.jsx
@@ -4,7 +4,8 @@ import './index.css'
 import App from './App.jsx'
 
 
-//the `createRoot` function  is a built-in function that is used to create a root node for a React application.
+//the `createRoot` function  is a built-in function 
+// that is used to create a root node for a React application.
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
