40ff90996624bb72dc9047ca95b3a6456e6cb254
Levi Njoroge <Levynjoroge1@gmail.com>
2025-08-27 23:37:26 +0300

    working on react

--- react_fr_w3/learning-classes/src/App.jsx
+++ react_fr_w3/learning-classes/src/App.jsx
@@ -5,8 +5,20 @@ function App() {
     constructor(name) {
       this.brand = name;
     }
-    function 
+    present (){
+      return "this is my new "+this.brand;
+    }
   }
+  class Model extends Car{
+    constructor(name, model){
+      super(name)
+      this.model = model
+    }
+    showCase(){
+      return this.present+" it is a "+this.model
+    }
+  }
+  const myNewTesla = new Model("Tesla", "CyberTruck")
   return (
     <>
       
