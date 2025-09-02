f5abfd4f325f8b9c18a8c6fafd3affc2c10a2c78
Levi Njoroge <Levynjoroge1@gmail.com>
2025-09-02 23:35:27 +0300

    working on react

--- react_fr_w3/intro/src/HelloWorld.jsx
+++ react_fr_w3/intro/src/HelloWorld.jsx
@@ -1,7 +1,7 @@
 //kijuaji kidogo apa na pale 😄
 // courtesy of brocode!
 // I stopped using his tutorials for react since they seemed outdated
-//  so here I am, retrying the whole thing
+//  so here I am, retrying the whole thing, hope
 
 function HelloWorld({
     name = "World"
