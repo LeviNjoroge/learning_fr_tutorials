24323d2b26fcfb5a8307eee8bfbe4af2a309259c
Levi Njoroge <Levynjoroge1@gmail.com>
2025-09-04 19:26:03 +0300

    working on react

--- react_fr_w3/learning_ES6/classes.js
+++ react_fr_w3/learning_ES6/classes.js
@@ -1,3 +1,5 @@
 // will be going through how classes work in ES6:
 // a class is a blueprint of what an object should look like
-// classes are used in object oriented programming, and are highly recommended since they 
+// classes are used in object oriented programming, and are highly recommended since they encourage code reusability and are easier to document and store
+
+// lets create a class of a car and make an object out of it:
