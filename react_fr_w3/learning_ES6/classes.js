aacd881732f28d2d6202b3f8400c3a89a6e4f663
Levi Njoroge <Levynjoroge1@gmail.com>
2025-09-04 19:24:45 +0300

    working on react

--- react_fr_w3/learning_ES6/classes.js
+++ react_fr_w3/learning_ES6/classes.js
@@ -1,1 +1,3 @@
 // will be going through how classes work in ES6:
+// a class is a blueprint of what an object should look like
+// classes are used in object oriented programming, and are highly recommended since they 
