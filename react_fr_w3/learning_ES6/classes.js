ee3814d8e4d40c7ccdd0f270bbc801f0c2b7707a
Levi Njoroge <Levynjoroge1@gmail.com>
2025-09-04 19:33:57 +0300

    working on react

--- react_fr_w3/learning_ES6/classes.js
+++ react_fr_w3/learning_ES6/classes.js
@@ -3,3 +3,21 @@
 // classes are used in object oriented programming, and are highly recommended since they encourage code reusability and are easier to document and store
 
 // lets create a class of a car and make an object out of it:
+class Car{
+    constructor(model, manufacturer, year){
+        this.model = model
+        this.manufacturer = manufacturer
+        this.year = year
+    }
+
+    // add a method to call out the car details:
+    callCar(){
+        return "This is a "+this.model+" manufatured by "+this.manufacturer+" in the year "+this.year;
+    }
+}
+
+myNewTesla = new Car("family car", "tesla inc", 2021);
+
+console.log(myNewTesla.callCar())
+
+// lets now 
