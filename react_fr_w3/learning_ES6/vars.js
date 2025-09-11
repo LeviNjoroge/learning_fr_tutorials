402b4f322c653cc85a99596a690433bc2cc5d7ad
Levi Njoroge <Levynjoroge1@gmail.com>
2025-09-11 16:13:33 +0300

    working on learning react

---
+++ react_fr_w3/learning_ES6/vars.js
@@ -0,0 +1,11 @@
+/*  we have three ways to declare a variable:
+1. var - has a function scope ***
+2. let - has a block scope
+3. const - has a block scope ***
+*/
+
+// var 
+function callBook(){
+    var book = "Kasuku";
+}
+console.log(book)
