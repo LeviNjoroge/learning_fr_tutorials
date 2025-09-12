c3904b6016788449970a8e82993a10fca85ecea2
Levi Njoroge <Levynjoroge1@gmail.com>
2025-09-12 23:09:32 +0300

    still learing react

--- react_fr_w3/learning_ES6/template_statements.js
+++ react_fr_w3/learning_ES6/template_statements.js
@@ -2,7 +2,7 @@ let name = "Joe"
 let age = 21
 let gender = "male"
 
-const message1 = name+" is a good "+(gender=="male")?"boy":"girl"+"\n"+
+const message1 = name+" is a good "+((gender=="male")?"boy":"girl")+"\n"+
                 "He is "+age+" years old";
 
 const message2 = `${name} is a good ${gender=="male"?"boy":"girl"}
