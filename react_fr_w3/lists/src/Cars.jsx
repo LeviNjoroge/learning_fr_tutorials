95af28a04ea4a3788f381f96e27afa52f2c7f7cb
Levi Njoroge <Levynjoroge1@gmail.com>
2025-10-01 23:35:36 +0300

    working on react:lists

--- react_fr_w3/lists/src/Cars.jsx
+++ react_fr_w3/lists/src/Cars.jsx
@@ -5,13 +5,14 @@ function Cars() {
     return(
         <ul>
             {cars.map( ()=>
-                {
-                    if ("Honda") {
-                        {<li>I have a green {car}</li>}
-                    } else {
-                        {<li>I have a {car}</li>}
-                    }
-                }
+                // {
+                //     if ("Honda") {
+                //         {<li>I have a green {car}</li>}
+                //     } else {
+                //         {<li>I have a {car}</li>}
+                //     }
+                // }
+                <li>I have a {car}</li>
             )}
         </ul>
     );
