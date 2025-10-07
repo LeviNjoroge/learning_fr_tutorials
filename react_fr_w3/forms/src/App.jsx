a6de938818877735de1e1a5cb3ccc5e6140d515f
Levi Njoroge <Levynjoroge1@gmail.com>
2025-10-07 22:32:31 +0300

    working on react:forms

--- react_fr_w3/forms/src/App.jsx
+++ react_fr_w3/forms/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <form action="" method="post">
       <label htmlFor="name">Enter your name: </label>
-      <input type="text" value={name} onChange={handl/>
+      <input type="text" value={name} onChange=/>
 
       <label htmlFor="name">Enter your age: </label>
       <input type="text" value={age} onChange={handleChange}/>
