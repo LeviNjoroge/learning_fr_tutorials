88482f749bd68f7ac78e0b558fdbedcbb15cdf6e
Levi Njoroge <Levynjoroge1@gmail.com>
2025-10-09 18:42:40 +0300

    working on react:forms

--- react_fr_w3/forms/src/App.jsx
+++ react_fr_w3/forms/src/App.jsx
@@ -12,11 +12,11 @@ function App() {
   return (
     <form>
       <h2>Reg form</h2>
-      <label htmlFor="name">Enter your name: </label>
-      <input type="text" value={name}/>
+      <label htmlFor="name">Enter your name: <span color="red">*</span></label>
+      <input type="text" value={name}/> <br />
 
       <label htmlFor="age">Enter your age: </label>
-      <input type="text" value={age}/>
+      <input type="text" value={age}/> <br />
 
       <label htmlFor="email">Enter your email: </label>
       <input type="text" value={email}/>
@@ -24,7 +24,7 @@ function App() {
       <input type="submit" value="Submit" onSubmit={handleChange}/>
 
       <p>
-        Hi {name}, your email is {email}, and DOB is {getYear(new Date) - age}
+        Hi {name}, your email is {email}, and age is {age}
       </p>
     </form>
   )
