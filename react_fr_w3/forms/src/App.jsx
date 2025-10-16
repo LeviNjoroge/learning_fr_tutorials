0ad48f06edf5cecfb4f9c67fb6243854cd3bccfe
Levi Njoroge <Levynjoroge1@gmail.com>
2025-10-16 17:43:18 +0300

    still learning react|forms:(

--- react_fr_w3/forms/src/App.jsx
+++ react_fr_w3/forms/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [values, setName] = useState({
     name: "Guest",
     age: "N/A",
-    email: "example@examplemail.com"
+    email: "guest@examplemail.com"
   });
 
   function handleChange(e){
