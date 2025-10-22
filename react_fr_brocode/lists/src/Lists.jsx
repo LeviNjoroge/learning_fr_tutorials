817cd57d0ed404a53149a8e354f2f21393768abe
Levi Njoroge <Levynjoroge1@gmail.com>
2025-10-22 20:43:31 +0300

    learning react: lists

--- react_fr_brocode/lists/src/Lists.jsx
+++ react_fr_brocode/lists/src/Lists.jsx
@@ -1,7 +1,13 @@
 
-
 function Lists() {
-    
+    const fruits = ["Apple", "Pineapple", "Kiwi", "Banana", "Coconut", "Orange", "Mango"];
+
+    const renderFruits = fruits.map(fruit => {
+        <li key={fruit}>{fruit}</li>
+    })
+    return(
+        <ul>{renderFruits}</ul>
+    );
 }
 
 export default Lists;
