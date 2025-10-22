cc653f2b4fb652d9d78660f745fb6bc286ef33e0
Levi Njoroge <Levynjoroge1@gmail.com>
2025-10-22 19:58:51 +0300

    learning react: conditional rendering

--- react_fr_brocode/conditional_rendering/src/Greeting.jsx
+++ react_fr_brocode/conditional_rendering/src/Greeting.jsx
@@ -1,6 +1,30 @@
 
-function Greeting(props){ // this can be easier dealt with using objects
+function Greeting({
+    userLoggedIn = False,
+    username = "Guest",
+}){ // this can be easier dealt with using objects
     // userLoggedIn={false} username="Raya"
+
+    const welcomeUser = username =>{
+        return(
+            <div className="welcome-user">
+                <h1>Hi {username}! Welcommen to la platform:)</h1>
+            </div>
+        );
+    }
+
+    const queryLogIn = username =>{
+        return(
+            <div className="query-login">
+                <h1>Please log in to continue!</h1>
+            </div>
+        );
+    }
+
+    return(
+        userLoggedIn ? welcomeUser : queryLogIn
+    );
+
 }
 
 export default Greeting
