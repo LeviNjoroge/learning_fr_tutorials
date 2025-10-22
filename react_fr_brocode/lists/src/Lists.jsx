f334acd150b30f807408d12d9171192992b43ebd
Levi Njoroge <Levynjoroge1@gmail.com>
2025-10-22 20:52:50 +0300

    learning react: lists

--- react_fr_brocode/lists/src/Lists.jsx
+++ react_fr_brocode/lists/src/Lists.jsx
@@ -2,11 +2,13 @@
 function Lists() {
     const fruits = ["Apple", "Pineapple", "Kiwi", "Banana", "Coconut", "Orange", "Mango"];
 
-    const renderFruits = fruits.map(fruit =>{
-        <li key={fruit}> {fruit} </li>
-    })
+    // if you wanna sort:
+    fruits.sort();
+
+    const renderFruits = fruits.map(fruit => <li> {fruit} </li>);
+
     return(
-        <ul>{renderFruits}</ul>
+        <ol>{renderFruits}</ol>
     );
 }
 
