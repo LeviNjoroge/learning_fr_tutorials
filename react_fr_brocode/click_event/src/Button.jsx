2b1904beb3ca3ef6eb3c789aebc86bc824e7d4a7
Levi Njoroge <Levynjoroge1@gmail.com>
2025-10-31 23:15:37 +0300

    learning react: click event

--- react_fr_brocode/click_event/src/Button.jsx
+++ react_fr_brocode/click_event/src/Button.jsx
@@ -2,16 +2,17 @@
 function Button(){
 
     let count = 0;  // initializing counter at zero
-    const eventHandler = () =>{
-        if(count <5){
-            count ++;
-            console.log(`You clicked me ${count} time(s)!`);
-        }
-        else{
-            console.log("That's enough! Stop clicking me!");
-        }
+    const eventHandler = () =>{ 
+        if(count <5){ 
+            count ++; 
+            console.log(`You clicked me ${count} time(s)!`); 
+        } 
+        else{ 
+            console.log("That's enough! Stop clicking me!"); 
+        } 
     }
 
+    const handleClick
     return (
         <>
             <button onClick={eventHandler}>Click Moi 🙂</button>
