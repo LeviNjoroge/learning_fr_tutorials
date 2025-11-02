6525f5a3b9b705cd50f3196918f615fc8eb4f78c
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-02 23:32:06 +0300

    learning react: click event

--- react_fr_brocode/click_event/src/Button.jsx
+++ react_fr_brocode/click_event/src/Button.jsx
@@ -13,7 +13,7 @@ function Button(){
     }
 
     //creatinga click event handler
-    const handleClick = (event) => console.log(eventtarget);
+    const handleClick = (event) => console.log(eventtarget.TextContent);
 
     return (
         <>
