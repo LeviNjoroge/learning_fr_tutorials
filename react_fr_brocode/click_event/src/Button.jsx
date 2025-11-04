01a9fa7ffcad4401b470223a10578ff49c05199e
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-04 14:12:03 +0300

    learning react: click event

--- react_fr_brocode/click_event/src/Button.jsx
+++ react_fr_brocode/click_event/src/Button.jsx
@@ -29,7 +29,7 @@ function Button(){
 
     return (
         <>
-            <button onClick={handleClick}>Click Moi 🙂</button>
+            <button onClick={(e) => handleCLick(e)}>Click Moi 🙂</button>
         </>
     );
 }
