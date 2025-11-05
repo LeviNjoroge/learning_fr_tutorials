53db2b4696defff73e59fff0f7e1c594af4cd9bf
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-05 14:44:03 +0300

    working on react: task under useState

--- react_fr_brocode/useState/src/Counter.jsx
+++ react_fr_brocode/useState/src/Counter.jsx
@@ -2,6 +2,28 @@ import React, { useState } from 'react';
 
 function Counter(){
 
+    const [count, setCount] = useState(0);
+
+    const decreaseCounter = () => {
+        setCount(count - 1);
+    }
+
+    const increaseCounter = () => {
+        setCount(count + 1);
+    }
+
+    const resetCounter = () => {
+        setCount(count = 0);
+    }
+
+    return(
+        <div className="counter">
+            <h1>{count}</h1>
+            <button onClick={decreaseCounter} title='decrease counter'>➖</button>
+            <button onClick={increaseCounter} title='increase counter'>➕</button>
+            <button onClick={resetCounter} title='reset counter'>♻</button>
+        </div>
+    );
 }
 
 export default Counter;
