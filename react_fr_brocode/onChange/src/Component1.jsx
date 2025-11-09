0811edf5c16f097606a0022ed3189b81a143439f
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-09 17:56:28 +0300

    working on react: onchange

--- react_fr_brocode/onChange/src/Component1.jsx
+++ react_fr_brocode/onChange/src/Component1.jsx
@@ -1,6 +1,6 @@
 // will be going through onChange event handler
 // this is mostly used in forms elements
-// 
+// what it does is that it triggers a function every time a value changes
 
 function Component1(){
 
