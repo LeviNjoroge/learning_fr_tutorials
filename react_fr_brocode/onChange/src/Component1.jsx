44a92a153fadfc941df5b60d0dda64016bcacd2a
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-10 06:42:13 +0300

    working on react: onchange

--- react_fr_brocode/onChange/src/Component1.jsx
+++ react_fr_brocode/onChange/src/Component1.jsx
@@ -11,7 +11,20 @@ function Component1(){
         <input type="text" name="name" id="name"/> <br />
         <label htmlFor="age">Age:</label>
         <input type="number" name="age" id="age" /> <br />
-        <textarea name="bio" id="bio" placeholder=""></textarea>
+        <input type="radio" name="gender" id="gender"/>
+        
+        <label htmlFor="school">Enter your school:</label>
+        <select name="school" id="school">
+          <option value="SIST">School of Information Science and Technology</option>
+          <option value="Law">School of Law</option>
+          <option value="Agric">School of Agriculture</option>
+          <option value="Health">School of Health Sciences</option>
+          <option value="SPAS">School of Pure and Applied Sciences</option>
+          <option value="SASS">School of Arts and Social Sciences</option>
+          <option value="other" title="Your school is not included? Write it below">other</option>
+        </select> <br />
+        <input type="text" name="otherSchools" id="otherSchools" disabled={true}/>
+        <textarea name="bio" id="bio" placeholder="Tell us a bit about yourself..."></textarea> <br />
       </form>
     </>
   );
