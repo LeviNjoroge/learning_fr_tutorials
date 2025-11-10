8d8cc5ee5ddc0b088f4675192285701dfa1ddd13
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-10 06:53:40 +0300

    working on react: onchange

--- react_fr_brocode/onChange/src/Component1.jsx
+++ react_fr_brocode/onChange/src/Component1.jsx
@@ -7,24 +7,39 @@ function Component1(){
   return(
     <>
       <form>
-        <label htmlFor="name">Name:</label>
-        <input type="text" name="name" id="name"/> <br />
-        <label htmlFor="age">Age:</label>
-        <input type="number" name="age" id="age" /> <br />
-        <input type="radio" name="gender" id="gender"/>
-        
-        <label htmlFor="school">Enter your school:</label>
-        <select name="school" id="school">
-          <option value="SIST">School of Information Science and Technology</option>
-          <option value="Law">School of Law</option>
-          <option value="Agric">School of Agriculture</option>
-          <option value="Health">School of Health Sciences</option>
-          <option value="SPAS">School of Pure and Applied Sciences</option>
-          <option value="SASS">School of Arts and Social Sciences</option>
-          <option value="other" title="Your school is not included? Write it below">other</option>
-        </select> <br />
-        <input type="text" name="otherSchools" id="otherSchools" disabled={true}/>
-        <textarea name="bio" id="bio" placeholder="Tell us a bit about yourself..."></textarea> <br />
+        <div className="name">
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" id="name"/> <br />
+        </div>
+        <div className="age">
+          <label htmlFor="age">Age:</label>
+          <input type="number" name="age" id="age" /> <br />
+        </div>
+        <div className="gender">
+          <label htmlFor="Gender:"></label>
+            <label htmlFor="Male">
+              <input type="radio" name="gender" id="gender" value="Male"/></label>
+            <label htmlFor="Female">
+              <input type="radio" name="gender" id="gender" value="Female"/></label>
+            <label htmlFor="Prefer not to say">
+              <input type="radio" name="gender" id="gender" value="N/A"/></label>
+        </div>
+        <div className="school">
+          <label htmlFor="school">Enter your school:</label>
+          <select name="school" id="school">
+            <option value="SIST">School of Information Science and Technology</option>
+            <option value="Law">School of Law</option>
+            <option value="Agric">School of Agriculture</option>
+            <option value="Health">School of Health Sciences</option>
+            <option value="SPAS">School of Pure and Applied Sciences</option>
+            <option value="SASS">School of Arts and Social Sciences</option>
+            <option value="other" title="Your school is not included? Write it below">other</option>
+          </select> <br />
+          <input type="text" name="otherSchools" id="otherSchools" disabled={true}/>
+        </div>
+        <div className="bio">
+          <textarea name="bio" id="bio" placeholder="Tell us a bit about yourself..."></textarea> <br />
+        </div>
       </form>
     </>
   );
