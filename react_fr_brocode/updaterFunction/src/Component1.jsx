e251295e88ba470518841a9008039e6706e09ac7
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-13 19:33:50 +0300

    more efficient way of updating state variable incoming

--- react_fr_brocode/updaterFunction/src/Component1.jsx
+++ react_fr_brocode/updaterFunction/src/Component1.jsx
@@ -1,13 +1,12 @@
 // an updater function is used maily when you need to 
 // do multiple state updates and asynchrounous functions
+// also, to ensure your code is future proof, you can use it
 import React, { useState } from 'react';
 
 function Component1(){
 
     const [name, setName] = useState("Guest");
-    function updateName(){
-        setName("Andrew Kibe");
-    }
+    
     // there is a better way to do the above
 
     return(
