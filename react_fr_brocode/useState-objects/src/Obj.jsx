200a9f55e2aff0735c60eb92766762588bffdb6d
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-14 09:01:52 +0300

    useState x updaterFunctions x objects

--- react_fr_brocode/useState-objects/src/Obj.jsx
+++ react_fr_brocode/useState-objects/src/Obj.jsx
@@ -2,8 +2,10 @@
 // this is an illustration of how to go about stateful variables when dealing with objects
 // to do this, you need to have an idea of:
 // useState - to create stateful variables and update them accordingly
-// 
+// updaterFunctions - used to carefully update stateful variables
+// objects - this is what we are dealing with
 
+// to begin we are going to create a 
 import React, { useState } from 'react';
 
 function Obj(){
