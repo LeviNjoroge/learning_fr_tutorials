0fa1adca7d811812fc6655eae944409ce316ec8b
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-17 18:40:07 +0300

    learning how to update the state of an array using react

--- react_fr_brocode/update_array/src/Foods.jsx
+++ react_fr_brocode/update_array/src/Foods.jsx
@@ -1,9 +1,20 @@
 import React, { useState } from 'react';
 
-
 const Foods = () =>{
+
+    const [foods, setFoods] = useState([]);
+
+    // function to remove an item from the array
+    function removeFood(){
+        setFoods()
+    }
+
+    // function to add an item to the array
+    function addFood(){
+        setFoods()
+    }
     return(
-        
+
     );
 }
 
