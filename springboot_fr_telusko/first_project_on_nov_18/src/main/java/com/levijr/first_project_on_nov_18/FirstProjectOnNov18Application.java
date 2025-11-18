5d6318de3a54c4cc2a34fb899d5ec312f9de01ba
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-18 17:28:57 +0300

    first commit on the first task with telusko

---
+++ springboot_fr_telusko/first_project_on_nov_18/src/main/java/com/levijr/first_project_on_nov_18/FirstProjectOnNov18Application.java
@@ -0,0 +1,13 @@
+package com.levijr.first_project_on_nov_18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectOnNov18Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstProjectOnNov18Application.class, args);
+    }
+
+}
---
+++ springboot_fr_telusko/first_project_on_nov_18/src/test/java/com/levijr/first_project_on_nov_18/FirstProjectOnNov18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levijr.first_project_on_nov_18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectOnNov18ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
