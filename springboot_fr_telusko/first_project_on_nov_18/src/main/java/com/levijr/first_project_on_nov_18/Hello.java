9e3524181a9ace48370ff4e8601b552f4c1e1944
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-18 18:38:15 +0300

    commiting some untracked work

--- springboot_fr_telusko/first_project_on_nov_18/src/main/java/com/levijr/first_project_on_nov_18/Hello.java
+++ springboot_fr_telusko/first_project_on_nov_18/src/main/java/com/levijr/first_project_on_nov_18/Hello.java
@@ -1,4 +1,24 @@
 package com.levijr.first_project_on_nov_18;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Hello {
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/{id}")
+    public String bananas(@PathVariable String id) {
+        return "I love "+id;
+    }
+
+//    public static void main(){
+//        System.out.println("Hello World!");
+//    }
+
 }
