509390a7803f93d7f4b7c6cd6fad8c72dc97c680
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-21 18:43:21 +0300

    learning some react: updating objects

--- react_fr_brocode/update_objects/src/Cars.jsx
+++ react_fr_brocode/update_objects/src/Cars.jsx
@@ -6,6 +6,7 @@ function Cars(){
     const [cars, setCars] = useState([]);
     const [carYear, setCarYear] = useState(new Date().getFullYear())
     const [carMake, setCarMake] = useState("");
+    const [carModel, setCarModel] = useState("");
 
     return(
         <>
