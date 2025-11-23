4cdcdba5a48587e3f186210728eea659d8dcf54b
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-23 21:04:08 +0300

    working on a mini project, the basic to do list app

--- react_fr_brocode/ToDoListApp_project/src/App.jsx
+++ react_fr_brocode/ToDoListApp_project/src/App.jsx
@@ -2,6 +2,7 @@
 // to access the first one, go to github.LeviNjoroge/ToDoList or a similar name, ie that repo is among the main ones there, youll spot it forsho
 // enjoy it, and that, and also try to see what new thing I have learnt while working on this one
 
+
 function App(){
 
   return (<>
