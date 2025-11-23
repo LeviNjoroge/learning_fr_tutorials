fc305dfc84b44d153d183e53bdddf804e336b47d
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-23 21:05:05 +0300

    working on a mini project, the basic to do list app

--- react_fr_brocode/ToDoListApp_project/src/App.jsx
+++ react_fr_brocode/ToDoListApp_project/src/App.jsx
@@ -2,7 +2,7 @@
 // to access the first one, go to github.LeviNjoroge/ToDoList or a similar name, ie that repo is among the main ones there, youll spot it forsho
 // enjoy it, and that, and also try to see what new thing I have learnt while working on this one
 
-// now lemme go on a date and will come back here in a few hours
+// now lemme go on a date and will come back here in a few hours or maybe tomorrow if all goes well ;)
 
 function App(){
 
