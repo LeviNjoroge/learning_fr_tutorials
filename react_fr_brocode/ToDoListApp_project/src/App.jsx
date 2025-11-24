8847059d44d4495a158537fa5feaf5548ef5701e
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-24 23:04:32 +0300

    working on a mini project, the basic to do list app

--- react_fr_brocode/ToDoListApp_project/src/App.jsx
+++ react_fr_brocode/ToDoListApp_project/src/App.jsx
@@ -4,7 +4,7 @@
 
 // now lemme go on a date and will come back here in a few hours or maybe tomorrow if all goes well ;)
 
-// here I am, the
+// here I am, the exact second day
 
 function App(){
 
