c27f2fef9a973715f695a572c1ca0ed24341a28c
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-25 19:40:53 +0300

    working on a mini project, the basic to do list app

--- react_fr_brocode/ToDoListApp_project/src/ToDOList.jsx
+++ react_fr_brocode/ToDoListApp_project/src/ToDOList.jsx
@@ -6,25 +6,22 @@ function ToDoList(){
     const [newTasks, setNewTasks] = useState();
 
     function addTasks(){
-        
     }
 
     function removeTasks(event){
+    }
+
+    function moveTaskUp(event){
+    }
+
+    function moveTasksDown(event){
 
     }
 
-    function moveTaskUp(){
-
-    }
-
-    function moveTasksDown(){
-
-    }
     return(
-        <>
-            <h2>To Do List App</h2>
-            
-        </>
+        <div className="ToDoListApp">
+
+        </div>
     );
 }
 
