848890c2c320152ef1a4eaf1285968077234c144
Levi Njoroge <Levynjoroge1@gmail.com>
2025-11-28 23:57:26 +0300

    working on a mini project, the basic to do list app

--- react_fr_brocode/ToDoListApp_project/src/ToDOList.jsx
+++ react_fr_brocode/ToDoListApp_project/src/ToDOList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 function ToDoList(){
 
-    const [tasks, setTasks] = useState(["Dummy Task 1", "Dummy Task 2"]);
+    const [tasks, setTasks] = useState(["Dummy Task 1", "Dummy Task 2", "Bummy Task 3"]);
     const [newTasks, setNewTasks] = useState();
 
     function addTasks(){
