4049ebdd8f41e3cf3a7ac067f2572f852552e016
Levi Njoroge <Levynjoroge1@gmail.com>
2025-12-02 21:21:04 +0300

    working on a mini project, the basic to do list app

--- react_fr_brocode/ToDoListApp_project/src/ToDOList.jsx
+++ react_fr_brocode/ToDoListApp_project/src/ToDOList.jsx
@@ -30,7 +30,7 @@ function ToDoList(){
                 <ul>
                     <li>Adding an item to the list</li>
                     <li>Removing an item from the List</li>
-                    <li>Managing its priority/urgency</li>
+                    <li>Managing its priority/urgency, </li>
                 </ul>
             </p>
         </div>
