ff78dd28c17471c00f38b542d8b5e5519bf7418c
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-12 22:19:47 +0300

    learning react

--- react_fr_brocode/learning_props/src/App.jsx
+++ react_fr_brocode/learning_props/src/App.jsx
@@ -2,7 +2,7 @@ import Student from "./Student";
 function App(){
   return(
     <>
-      {/** */
+      {/* */}
       <Student name="Spongebob" age={30} course="Food Processing"/>
       <Student name="Patrick" age={36} course="Fishology" title="if there exists LOL"/>
       <Student name="Sandy" age="21" course="BSc. Physics"/>
