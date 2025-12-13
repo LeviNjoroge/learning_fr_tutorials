129748d3764312511f2a37f8acd1070f58bd5281
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-14 00:00:27 +0300

    learning react

--- react_fr_brocode/learning_props/src/App.jsx
+++ react_fr_brocode/learning_props/src/App.jsx
@@ -8,7 +8,7 @@ function App(){
       <Student name="Sandy" age="21" course="BSc. Physics"/>
       <Student/>
       <Student name="Squidward" age="90" course="Music"/>
-      <Student name="Mr. Crabs" />
+      <Student name="Mr. Crabs" age={}/>
     </>
   );
 }
