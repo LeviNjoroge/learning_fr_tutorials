76bcae8b46f36c161a186e7cda085df349b569a1
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-20 22:57:26 +0300

    learning react

--- react_fr_brocode/useEffects/src/Component1.jsx
+++ react_fr_brocode/useEffects/src/Component1.jsx
@@ -1,6 +1,6 @@
 // this' one of the many REACT HOOKS that exist out there
 // Use Effect is the second Hook we'll be going through in this journey of learning react
-// with 
+// with useEffect hooh, 
 
 import React, { useState, useEffect } from 'react';
 
