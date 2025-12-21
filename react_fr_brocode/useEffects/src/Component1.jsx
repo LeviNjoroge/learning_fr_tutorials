a4cac69b65123bcc952497594cca64a71e113f44
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-21 20:35:28 +0300

    learning react

--- react_fr_brocode/useEffects/src/Component1.jsx
+++ react_fr_brocode/useEffects/src/Component1.jsx
@@ -1,7 +1,3 @@
-// this' one of the many REACT HOOKS that exist out there
-// Use Effect is the second Hook we'll be going through in this journey of learning react
-// with useEffect hook, its like telling the program to execute some code when some specific activity happens
-// we will go deep into this later in this series
 
 import React, { useState, useEffect } from 'react';
 
