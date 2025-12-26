ca3738e2a76f67ab744d05aa505b86afb8f5cf38
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-26 08:52:30 +0300

    learning react: useEffects

--- react_fr_brocode/useEffects/src/Component1.jsx
+++ react_fr_brocode/useEffects/src/Component1.jsx
@@ -6,10 +6,15 @@ import React, { useState, useEffect } from 'react';
 function Component1(){
     const [count, setCount] = useState(0);
 
+    function addCount() {
+        setCount(c => c+1);
+    }
+
     return (
 
         <>
             <p>{count}</p>
+            <button>Add</button>
         </>
     );
 }
