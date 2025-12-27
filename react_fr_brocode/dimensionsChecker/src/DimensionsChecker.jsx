8f396de924f67583bb7a91ab18d2b0f9fa6aa829
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-27 21:30:13 +0300

    working on react:device dimensions checker

--- react_fr_brocode/dimensionsChecker/src/DimensionsChecker.jsx
+++ react_fr_brocode/dimensionsChecker/src/DimensionsChecker.jsx
@@ -5,11 +5,22 @@ function DimensionsChecker(){
     const [width, setWidth] = useState(window.innerWidth);
     const [height, setHeight] = useState(window.innerHeight);
 
+    function handleWidthChange(){
+        useEffect(()=>{
+            setWidth(window.innerWidth);
+        }, [window.innerWidth]);
+    }
 
-
+    function handleWidthChange(){
+        useEffect(()=>{
+            setWidth(window.innerWidth);
+        }, [window.innerWidth]);
+    }
+    
     return (
         <>
-            
+            Width: {width}px
+            Height: {height}px
         </>
     );
 }
