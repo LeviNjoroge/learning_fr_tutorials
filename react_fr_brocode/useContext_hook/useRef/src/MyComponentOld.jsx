fda929a573d016263f1ca365ed32b2fa67ad251f
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-30 20:04:28 +0300

    learning react, still: useRef

---
+++ react_fr_brocode/useContext_hook/useRef/src/MyComponentOld.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef} from 'react';
+
+function MyComponentOld(){
+
+    return(
+        
+        <>
+            
+        </>
+    );
+}
+
+export default MyComponentOld;
