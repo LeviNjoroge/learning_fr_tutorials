d55400d9077e6f4af0ac6107771b7e8f1d469277
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-30 12:27:06 +0300

    learning react, still: useEffects

--- react_fr_brocode/digital_clock/src/DigitalClock.jsx
+++ react_fr_brocode/digital_clock/src/DigitalClock.jsx
@@ -1,12 +1,14 @@
+import React, { useState, useEffect } from 'react';
 
 function DigitalClock(){
 
     return(
 
-        <>
-        
-        </>
+        <div className="digitalClock">
+            <a href="https://www.freepik.com/free-ai-image/sunrise-snowy-mountains-clouds_420708233.htm">Image by freepik</a>
+        </div>
     );
 }
 
 export default DigitalClock;
+
