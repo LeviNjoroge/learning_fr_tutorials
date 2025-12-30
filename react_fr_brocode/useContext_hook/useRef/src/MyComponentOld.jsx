e9b352b68c9b258a8df13f275f7970cc716c7782
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-30 20:06:03 +0300

    learning react, still: useRef

--- react_fr_brocode/useContext_hook/useRef/src/MyComponentOld.jsx
+++ react_fr_brocode/useContext_hook/useRef/src/MyComponentOld.jsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect, useRef} from 'react';
 
 function MyComponentOld(){
 
+    let [number, setNumber] = useState(0)
+
     return(
-        
         <>
-            
+            <button onClick={handleClick}>Click Moi:)</button>
         </>
     );
 }
