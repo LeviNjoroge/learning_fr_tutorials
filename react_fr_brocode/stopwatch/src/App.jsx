6f3ee0256ff10a06bf30271ec24f04ff75d498c3
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-30 22:12:05 +0300

    working on react, still: stopwatch

--- react_fr_brocode/stopwatch/src/App.jsx
+++ react_fr_brocode/stopwatch/src/App.jsx
@@ -4,6 +4,11 @@ function App() {
 
   return (
     <>
+      <h2>Stopwatch 1</h2>
+      <Stopwatch/>
+      <h2>Stopwatch 2</h2>
+      <Stopwatch/>
+      <h2>Stopwatch 3</h2>
       <Stopwatch/>
     </>
   )
