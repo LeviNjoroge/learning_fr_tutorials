3abd4f49e6fb4e567b0455dd49276bb1004a9810
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-30 20:21:29 +0300

    learning react, still: useRef

--- react_fr_brocode/useContext_hook/useRef/src/MyComponentOld.jsx
+++ react_fr_brocode/useContext_hook/useRef/src/MyComponentOld.jsx
@@ -4,6 +4,9 @@ function MyComponentOld(){
 
     let [number, setNumber] = useState(0)
 
+    function handleClick(){
+        setNumber(n => n+1);
+    }
     return(
         <>
             <button onClick={handleClick}>Click Moi:)</button>
