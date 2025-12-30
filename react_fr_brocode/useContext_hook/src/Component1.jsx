4f4256d7b460bd84833fbdeb4f072f213ac99fd0
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-30 13:57:59 +0300

    learning react, still: useContext

--- react_fr_brocode/useContext_hook/src/Component1.jsx
+++ react_fr_brocode/useContext_hook/src/Component1.jsx
@@ -1,5 +1,5 @@
 import Component2 from "./Component2";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext} from 'react';
 
 function Component1(){
 
