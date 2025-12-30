c11c9aa6b88d4485baf0f4348037c8d984804dfc
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-30 20:26:41 +0300

    learning react, still: useRef

--- react_fr_brocode/useContext_hook/useRef/src/MyComponent.jsx
+++ react_fr_brocode/useContext_hook/useRef/src/MyComponent.jsx
@@ -1,11 +1,23 @@
-import React, { useState, useEffect, useRef} from 'react';
+import React, {useEffect, useRef} from 'react';
 
 function MyComponent(){
+    // create a constant and assign a value to it
+    const ref = useRef(0);
+
+    // check for when the component rerenders
+    useEffect(()=>{
+        console.log('COMPONENT RERENDERED')
+    })
+
+    function handleAddition(){
+        ref.current++;
+    }
 
     return(
         
         <>
-        
+            <h1>{ref}</h1>
+            <button onClick={handleAddition}>Increase Value</button>
         </>
     );
 }
