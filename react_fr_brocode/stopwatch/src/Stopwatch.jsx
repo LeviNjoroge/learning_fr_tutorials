de29e94c1f1e1aa730016b2d1a8add360d4a7a21
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-30 21:26:27 +0300

    working on react, still: stopwatch

--- react_fr_brocode/stopwatch/src/Stopwatch.jsx
+++ react_fr_brocode/stopwatch/src/Stopwatch.jsx
@@ -3,10 +3,40 @@ import React, { useState, useEffect, useRef } from 'react';
 
 function Stopwatch() {
 
+    const [isRunning, setIsRunning] = useState(false);
+    const [elapsedTime, setElapsedTime] = useState(0);
+
+    const intervalIdRef = useRef(null)
+    const startTimeRef = useRef(0);
+
+    useEffect(()=>{
+
+    },[isRunning])
+
+    function start(){
+
+    }
+
+    function stop(){
+
+    }
+
+    function reset(){
+
+    }
+
+    function formatTime(){
+        return `00:00:00`;
+    }
   return (
-    <>
-      
-    </>
+    <div className="stopWatch">
+        <div className="display">{formatTime()}</div>
+        <div className="controls">
+            <button className='startButton' onClick={start}>Start</button>
+            <button className='stopButton' onClick={stop}>Stop</button>
+            <button className='resetButton' onClick={reset}>Reset</button>
+        </div>
+    </div>
   )
 }
 
