ad4a809d36e5f0ae1d6a1fb19c6ab6a88a57b462
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-30 13:27:43 +0300

    learning react, still: useEffects

--- react_fr_brocode/digital_clock/src/DigitalClock.jsx
+++ react_fr_brocode/digital_clock/src/DigitalClock.jsx
@@ -1,12 +1,41 @@
 import React, { useState, useEffect } from 'react';
+import Snowfall from 'react-snowfall';
 
 function DigitalClock(){
 
-    return(
+    const [time, setTIme] = useState(new Date());
+    
+    useEffect(()=>{
+        const intervalId = setInterval(() => {
+            setTIme(new Date())
+        }, 1000);
 
+        return () =>{
+            clearInterval(intervalId);
+        }
+    }, [])
+
+    const formatTime = ()=>{
+        let hours = time.getHours();
+        let mins = time.getMinutes();
+        let secs = time.getSeconds();
+        let meridiem = hours >= 12? "PM": "AM";
+
+        hours = hours%12 || 12;
+
+        return `${padZero(hours)}:${padZero(mins)}:${padZero(secs)} ${meridiem}`
+    }
+
+    function padZero(number){
+        return (number<10?"0":"")+number;
+    }   
+
+    return(
         <div className="digitalClock">
+            <Snowfall color="white"/>
+
             <div className="clock">
-                <span>00:00:00</span>
+                <span>{formatTime()}</span>
             </div>
             <footer><a href="https://www.freepik.com/free-ai-image/sunrise-snowy-mountains-clouds_420708233.htm">Image by freepik</a></footer>
         </div>
