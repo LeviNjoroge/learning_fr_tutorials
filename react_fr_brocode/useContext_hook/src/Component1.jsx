b94ffa89500c5a4f011a044877c7d7db5b1a0448
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-30 13:50:31 +0300

    learning react, still: useContext

--- react_fr_brocode/useContext_hook/src/Component1.jsx
+++ react_fr_brocode/useContext_hook/src/Component1.jsx
@@ -1,10 +1,14 @@
 import Component2 from "./Component2";
+import React, { useState, useEffect } from 'react';
 
 function Component1(){
+
+    const [user, setUser] = useState("Jay");
 
     return(
         <div className="box">
             <h1>Component1</h1>
+            <h2>Hello {user}</h2>
             <Component2/>
         </div>
     );
