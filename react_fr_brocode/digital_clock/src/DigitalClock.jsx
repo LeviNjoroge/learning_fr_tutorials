e7e71d6a94c8c24e0af23f2631a1a193dcc129ee
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-30 12:44:01 +0300

    learning react, still: useEffects

--- react_fr_brocode/digital_clock/src/DigitalClock.jsx
+++ react_fr_brocode/digital_clock/src/DigitalClock.jsx
@@ -8,7 +8,7 @@ function DigitalClock(){
             <div className="clock">
                 <span>00:00:00</span>
             </div>
-            <a href="https://www.freepik.com/free-ai-image/sunrise-snowy-mountains-clouds_420708233.htm">Image by freepik</a>
+            <footer><a href="https://www.freepik.com/free-ai-image/sunrise-snowy-mountains-clouds_420708233.htm">Image by freepik</a></footer>
         </div>
     );
 }
