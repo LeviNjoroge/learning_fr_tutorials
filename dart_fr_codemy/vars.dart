3b0abd8a1d19a0c958b89479bd188b0eb4b10bf9
Levi Njoroge <levynjoroge1@gmail.com>
2026-01-02 14:22:07 +0300

    learning dart: vars

--- dart_fr_codemy/vars.dart
+++ dart_fr_codemy/vars.dart
@@ -4,9 +4,9 @@ void main(){
 
   int age = 30; // BEST PRACTICE, if you know the data type you are working with, use the datatype name to declare the variable
 
-  dynamic gpa = 3.5;
+  dynamic gpa = 3.5; // declaring a variable using 'dynamic'
 
-  const String school = "Gumbaru School";
+  const String school = "Gumbaru School"; // setting a constant
   // school = "NBO Gumbaru School"; -- DOES NOT WORK SINCE THE VARIABLE IS A CONSTANT
   name = "Joseph"; // see how the value of the variable 'name' accepts change
 
