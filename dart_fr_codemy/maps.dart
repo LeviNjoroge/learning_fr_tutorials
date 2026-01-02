d856c058617fc170853e78d229ea6055eb7172f8
Levi Njoroge <levynjoroge1@gmail.com>
2026-01-02 21:35:15 +0300

    learning dart: maps

--- dart_fr_codemy/maps.dart
+++ dart_fr_codemy/maps.dart
@@ -1,10 +1,21 @@
 void main(){
 
   // Maps
+  // declare using either var or Map
+  var brand = {"Mary":"Toyota","Elon":"Tesla"};
+  Map toppings = {"Lee":"Pepperoni", "John":"Cheese"};
 
-  Map toppings = {"Lee":"Pepperoni"};
-  print("Toppings: ${toppings}");
+
+  print("Toppings: ${toppings}"); // acccess the whole map
+  print("John's fav topping is ${toppings["John"]}"); // get the value of an item in the map
+  print("All registered users: ${toppings.keys}"); // get all keys
+  print("All requested toppings: ${toppings.values}"); // get all values
+  print("Number of registered toppings: ${toppings.length}"); // get the number of key/value pairs existing
+
+  // to add an item 
 
   Map<String, String> namesAndCourses = {};
   print(namesAndCourses);
+
+  
 }
