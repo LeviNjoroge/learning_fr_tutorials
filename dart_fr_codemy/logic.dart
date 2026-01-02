fac02478eaf5d524d4e0eee4e32ce0d8bd35cc25
Levi Njoroge <levynjoroge1@gmail.com>
2026-01-02 22:22:57 +0300

    learning dart: logic

---
+++ dart_fr_codemy/logic.dart
@@ -0,0 +1,17 @@
+void main(){
+
+  // Logic
+  // if
+  var isStudent = true;
+  if(isStudent){
+    print("Tee is a student!");
+  }
+
+  var isCitizen = false;
+  if(isCitizen){
+    print("Tee is a citizen");
+  } else{
+    print("Tee is an international student");
+  }
+
+}
