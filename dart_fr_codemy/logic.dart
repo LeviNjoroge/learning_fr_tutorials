c989b6e43d2567a3eb293e89998eba8fd2a6df15
Levi Njoroge <levynjoroge1@gmail.com>
2026-01-02 22:29:12 +0300

    learning dart: logic

--- dart_fr_codemy/logic.dart
+++ dart_fr_codemy/logic.dart
@@ -1,17 +1,27 @@
 void main(){
 
+  String studentName = "Tom";
   // Logic
   // if
   var isStudent = true;
   if(isStudent){
-    print("Tee is a student!");
+    print("${studentName} is a student!");
   }
 
+  // if else
   var isCitizen = false;
   if(isCitizen){
-    print("Tee is a citizen");
+    print("${studentName} is a citizen");
   } else{
-    print("Tee is an international student");
+    print("${studentName} is an international student");
   }
 
+  var gpa = 3.5;
+  if(gpa <2){
+    print("${studentName} needs to work on his gpa");
+  } else if(gpa < 3.5){
+    print("${studentName} is doing okay");
+  } else{
+    print("${studentName} has a really good gpa, he's passing");
+  }
 }
