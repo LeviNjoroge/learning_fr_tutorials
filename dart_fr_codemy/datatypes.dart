19b1de10614341ffd1358838e89dae483c46a9a0
Levi Njoroge <levynjoroge1@gmail.com>
2026-01-02 15:23:05 +0300

    learning dart: data types

---
+++ dart_fr_codemy/datatypes.dart
@@ -0,0 +1,17 @@
+void main(){
+
+  // DATATYPES
+  // Strings
+  String name = "Joe";
+  print("String $name");
+
+  //Integers
+  int age = 20;
+  print("Integer: $age");
+
+  // Double
+  double gpa = 4.2;
+  print("Double: $gpa");
+
+  
+}
