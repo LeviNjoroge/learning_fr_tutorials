e632faae96d0b96adaa303efda3f58692e9fb799
Levi Njoroge <levynjoroge1@gmail.com>
2026-01-02 14:20:14 +0300

    learning dart: vars

--- dart_fr_codemy/vars.dart
+++ dart_fr_codemy/vars.dart
@@ -1,12 +1,21 @@
 void main(){
 
-  var name = "Joe";
+  var name = "Joe"; //declaring a variable using 'var'
+  name = "Joseph"; // see how the value of the variable 'name' accepts change
 
   int age = 30;
 
   dynamic gpa = 3.5;
 
+  const String school = "Gumbaru School";
+  // school = "NBO Gumbaru School"; -- DOES NOT WORK SINCE THE VARIABLE IS A CONSTANT
+
+  int finalScore; // you can declare and not assign
+  finalScore = 45;
+
   print(name);
   print(age);
   print(gpa);
+  print(school);
+  print(finalScore);
 }
