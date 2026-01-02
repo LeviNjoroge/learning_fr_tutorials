74415c641bf4b3cce66014926e98cca8755bf486
Levi Njoroge <levynjoroge1@gmail.com>
2026-01-02 23:11:23 +0300

    learning dart: functions

--- dart_fr_codemy/functions.dart
+++ dart_fr_codemy/functions.dart
@@ -7,4 +7,16 @@ void main(){
   }
 
   print(myFunc());
+
+   // say hi to the user
+  greeting(String name){
+    return "Hello ${name}";
+  }
+
+  print(greeting("Philip"));
+
+  introOnGuest(String name, homeTown, int age, [zipCode]){
+    return "Welcome ${age} yr old ${name}, from ${homeTown} ${zipCode}!";
+  }
+  print(introOnGuest("Joseph", "Matare", 22));
 }
