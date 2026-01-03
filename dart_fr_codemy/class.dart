817fdc611af31fd88da6d5ac8857058e63908cf5
Levi Njoroge <levynjoroge1@gmail.com>
2026-01-03 23:05:28 +0300

    learning dart: classes

--- dart_fr_codemy/class.dart
+++ dart_fr_codemy/class.dart
@@ -1,4 +1,20 @@
 
 void main(){
-  
+  Person Joe =Person("John", "Male", 20);
+  print(Joe.displayData("white"));
+}
+
+class Person{
+  String? name, sex;
+  int? age;
+
+  Person(String name,String sex,int age){
+    this.name = name;
+    this.sex = sex;
+    this.age = age;
+  }
+
+  String displayData(String race){
+    return "Name: ${name}\nSex: ${sex}\nAge: ${age} is ${race}";
+  }
 }
