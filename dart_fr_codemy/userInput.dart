83b0e529e2e29fcc8a4258ff0aa845715f19a59b
Levi Njoroge <levynjoroge1@gmail.com>
2026-01-03 15:55:26 +0300

    learning dart: user input

--- dart_fr_codemy/userInput.dart
+++ dart_fr_codemy/userInput.dart
@@ -5,7 +5,7 @@ void main(){
   // user Input
   print("Enter your name");
 
-  // can declare a variable with 
+  // can declare a variable with var, dynamic, or nullable type
   String? name = stdin.readLineSync();
 
   print("Hello ${name}");
