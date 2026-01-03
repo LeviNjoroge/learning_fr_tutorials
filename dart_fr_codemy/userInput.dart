cd3664063aeb55e06a91fb43872d858076c05fd2
Levi Njoroge <levynjoroge1@gmail.com>
2026-01-03 15:55:09 +0300

    learning dart: user input

--- dart_fr_codemy/userInput.dart
+++ dart_fr_codemy/userInput.dart
@@ -1,6 +1,12 @@
-
+import 'dart:io';
 
 void main(){
 
+  // user Input
+  print("Enter your name");
 
+  // can declare a variable with 
+  String? name = stdin.readLineSync();
+
+  print("Hello ${name}");
 }
