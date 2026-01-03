c18dbc358a5df08234874a47a6d6c32481a35b4b
Levi Njoroge <levynjoroge1@gmail.com>
2026-01-03 17:15:22 +0300

    practicing Dart: FizzBuzz

--- dart_fr_codemy/FizzBuzz.dart
+++ dart_fr_codemy/FizzBuzz.dart
@@ -6,5 +6,7 @@ void main(){
   
   // approach, first check whether the number is divisible by both 3 and 5 
   // then check if divisible by either
-  
+  for (var i = 1; i<=100;i++){
+    print(i);
+  }
 }
