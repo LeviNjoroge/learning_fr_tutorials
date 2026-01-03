083d0aa28a581ecfc6a2a600763e9cf851714769
Levi Njoroge <levynjoroge1@gmail.com>
2026-01-03 16:22:33 +0300

    learning dart: user input type convertions

--- dart_fr_codemy/userInputTypeConversion.dart
+++ dart_fr_codemy/userInputTypeConversion.dart
@@ -1,6 +1,18 @@
-
+import 'dart:io';
 
 void main(){
 
   // user input type conversion
+
+  print("Hello! Enter name please:");
+  String? name = stdin.readLineSync();
+  print("Nice to meet you ${name}\n How old are you?");
+  var age = stdin.readLineSync();
+  age? = int.parse(age);
+  if (age > 18) {
+    print("Welcome to the site $name");
+  }
+  else{
+    print("Sorry ${name}, you are too young to access this site!");
+  }
 }
