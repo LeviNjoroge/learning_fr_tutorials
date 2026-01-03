e4c258a0f7d6ae8d52ffb7193242ad8820967c96
Levi Njoroge <levynjoroge1@gmail.com>
2026-01-03 16:13:01 +0300

    learning dart: convertions

---
+++ dart_fr_codemy/convert.dart
@@ -0,0 +1,20 @@
+
+
+void main(){
+  
+  // convert string to integer
+  var a, b, c;
+  a=10;
+  b="32";
+  c = a + int.parse(b);
+  print(c);
+
+  // str to double
+  var d, e, f;
+  d = "3.14";
+  e = 40;
+  f = 40 * double.parse(d);
+  print(f);
+
+  // int or 
+}
