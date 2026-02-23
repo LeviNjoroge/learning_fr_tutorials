6d5b33fd8e31c8765e3944504fae4d190e8c17ff
LeviNjoroge <levynjoroge1@gmail.com>
2026-02-23 21:15:59 +0300

    learning next.js

---
+++ Next.js/first_next_app/app/Components/AddToCart.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <div>
+        <button className='AddToButton' onClick={()=>console.log("Button is clicked")}>Click me!</button>
+    </div>
+  )
+}
+
+export default AddToCart
--- Next.js/first_next_app/app/page.tsx
+++ Next.js/first_next_app/app/page.tsx
@@ -1,12 +1,14 @@
 import { link } from "fs";
 import Image from "next/image";
 import Link from "next/link";
+import AddToCart from "./Components/AddToCart";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
       <Link href="./users">Users</Link>
+      <AddToCart/>
     </main>
-  );
+  )
 }
