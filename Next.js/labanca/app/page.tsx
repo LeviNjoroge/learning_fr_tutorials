3e4398477c21682874064cc25ebb9cea3d210ac4
LeviNjoroge <levynjoroge1@gmail.com>
2026-02-28 16:05:24 +0300

    initial commit on the labanca project

---
+++ Next.js/labanca/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="labanca_landing">
+
+      <div className="hero">
+        <h1>Welcome to Labanca</h1>
+        <p>The future of banking</p>
+      </div>
+
+      
+    </div>
+  );
+}
