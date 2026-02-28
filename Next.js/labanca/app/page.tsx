d93887d14e9c88a35e33f5c2a6e4af6b24a9f2aa
LeviNjoroge <levynjoroge1@gmail.com>
2026-02-28 17:03:34 +0300

    working on a new project while learning Nextjs

--- Next.js/labanca/app/page.tsx
+++ Next.js/labanca/app/page.tsx
@@ -4,11 +4,32 @@ export default function Home() {
   return (
     <div className="labanca_landing">
 
-      <div className="hero">
-        <h1>Welcome to Labanca</h1>
-        <p>The future of banking</p>
+      <div id="hero">
+        <h1 className="text-center text-4xl font-bold mt-1">Labanca.. the future of banking</h1>
+      </div>
+      <div id="mantra">
+        <div id="mission">
+          At Labanca, our mission is to ease banking so that whether it is transferring it, depositing, and withdrawing, your experiense is seamless and fast.
+        </div>
+        <div id="vision">
+          Our vision is to have the fastest and most reliable banking system in the space of finance.
+        </div>
       </div>
 
+      <div id="our_services">
+        <div id="deposits">
+          <h1>Deposits</h1>
+          You can deposit your money into your account at any of our branches available country-wide, via our agents, and using our 24Hours operational ATMs
+        </div>
+        <div id="withdrawals">
+          <h1>Withdrawals</h1>
+          At labanca, you can have money in the palm of your hands anytime, through our well distributed bank branches, agent shops and ATMs. 
+        </div>
+        <div id="transfers">
+          <h1>Transfers</h1>
+          Wherever you are, no matter the time, you can send money to your friends and famil
+        </div>
+      </div>
       
     </div>
   );
