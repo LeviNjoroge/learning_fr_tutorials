f2c85855d96a9d3f293b19166186f642c57f471c
LeviNjoroge <levynjoroge1@gmail.com>
2026-03-13 18:23:53 +0300

    learning py

--- Python/Py_from_mosh/learning_py_fnctions.py
+++ Python/Py_from_mosh/learning_py_fnctions.py
@@ -3,4 +3,21 @@ def greetings(name):
     print(f"Hello {name}")
 
 
+def get_greeting(name):
+    return f"Hello {name}"
+
+
+def get_details(name="Guest", age=18):
+    return name, age
+
+
+def sum_of_many(*numbers):
+    sum = 0
+    for number in numbers:
+        sum += number
+    return sum
+
+
+print(sum_of_many(12, 32, 43, 123, 34))
 greetings("Levi")
+print(get_details())
