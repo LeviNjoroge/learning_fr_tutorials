57270552d439a66f083bf57bbd18df1eb799f6b8
LeviNjoroge <levynjoroge1@gmail.com>
2026-03-13 14:49:20 +0300

    learning py

---
+++ Python/Py_from_mosh/looping.py
@@ -0,0 +1,10 @@
+# for loop
+for i in range(3):
+    print("Yello")
+
+fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew","incaberry", "jackfruit", "kiwi", "lemon", "mango", "nectarine", "orange", "papaya", "quince", "raspberry", "strawberry", "tangerine", "ugli fruit","victoria plum", "watermelon", "xigua", "yellow passion fruit", "zucchini"]
+print(len(fruits))
+for some_fruits in range(2, 5):
+    print(fruits[some_fruits])
+for fruit in fruits:
+    print(fruit)
